7565e25752fbdebc3901e53efe89aec025a1b10e
Daniel Fernandes Silva <danielfs.ti@gmail.com>
2017-01-01 16:03:51 -0200

    Adicionando parâmetro -x

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export WORKDIR=/go/src/github.com/danielfs/${PWD##*/}
-docker run --rm -v "$PWD":${WORKDIR} -w ${WORKDIR} -e CGO_ENABLED=0 -e GOOS=linux golang:alpine go build -a -installsuffix cgo -o encurtador .
+docker run --rm -v "$PWD":${WORKDIR} -w ${WORKDIR} -e CGO_ENABLED=0 -e GOOS=linux golang:alpine go build -x -a -installsuffix cgo -o encurtador .
 docker build -t danielfs/encurtador .
